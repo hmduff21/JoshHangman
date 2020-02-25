2940d79d3162dfa20fc300384d62e7d41fa28f0d
Mr. Phiphe <60886383+hmduff21@users.noreply.github.com>
2020-02-25 09:04:47 -0500

    I added my broken code
    nice

--- Thing Thing.py
+++ Thing Thing.py
@@ -22,6 +22,26 @@ def main():
     ifWon ()
 main()
 
+updatedWord = [] 
+def test():
+    global secret
+    life = 6
+    guess = raw_input('Guess a letter: ')
+    updatedWord = []
+    if guess in secret:
+        updatedWord += 
+        print(updatedWord)
+        
+        
+    if updatedWord == secret:
+        ifWon()
+    if life == 0:
+        print('You lost buckwheat \n Get em next time' )
+    else:
+        life - 1
+        getLetter()
+
+
 #def tester():
     #global updateWord
     #updateWord = raw_input()
